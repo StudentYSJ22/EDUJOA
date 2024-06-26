4c2a3b287d67880b68cf92b123d861d48ffc8946
chs53 <chs53@DESKTOP-NKFMJQF>
2024-06-26 14:38:31 +0900

    프로젝트 기초 설정 - chs

---
+++ src/main/java/com/edujoa/EdujoaApplication.java
@@ -0,0 +1,13 @@
+package com.edujoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdujoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdujoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edujoa/EdujoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edujoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdujoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
