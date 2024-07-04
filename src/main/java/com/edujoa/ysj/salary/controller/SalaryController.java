c8917cfb413d0aa4dcce63cfd329866a4f48b3a4
yuseonjeong <yuseonjeong@localhost>
2024-07-04 15:50:01 +0900

    ysj_ salary 작성 중

---
+++ src/main/java/com/edujoa/ysj/salary/controller/SalaryController.java
@@ -0,0 +1,27 @@
+package com.edujoa.ysj.salary.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.edujoa.chs.employee.model.dto.Employee;
+import com.edujoa.ysj.salary.model.service.SalaryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/salary")
+@RequiredArgsConstructor
+public class SalaryController {
+    private final SalaryService service;
+
+    @GetMapping("/salary.do")
+    public String salary(Model model) {
+        List<Employee> salaryList = service.getAllSalary();
+        model.addAttribute("salaryList", salaryList);
+        return "ysj/salary";
+    }
+}
